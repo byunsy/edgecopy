3c693f16b5c3c6a4bdc5117105888b6578d73320
Sang Yoon <bsyiskl@gmail.com>
2025-06-24 00:55:19 -0400

    Add scripts for test-run

---
+++ test-dataset/cram_to_bam.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SAMPLE=$1
+REF="hg38.fa"
+
+CRAM_FILE="${SAMPLE}.alt_bwamem_GRCh38DH.20150826.CEU.exome.cram"
+BAM_FILE="${SAMPLE}.CEU.exome.bam"
+
+echo "samtools view -b -T ${REF} -o ${BAM_FILE} ${CRAM_FILE}"
+samtools view -b -T ${REF} -o ${BAM_FILE} ${CRAM_FILE}
+
+echo "samtools index ${BAM_FILE}"
+samtools index ${BAM_FILE}
---
+++ test-dataset/create_bam_paths.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for BAM_FILE in $PWD/*.CEU.exome.bam; do
+    SAMPLE_NAME=$(basename "$BAM_FILE" .CEU.exome.bam)
+    echo "$BAM_FILE::$SAMPLE_NAME"
+done
+
