ff32faa2b964ac6d86094119f35b87419b5e9877
Sang Yoon <bsyiskl@gmail.com>
2025-06-24 14:23:02 -0400

    Fix small error in filepath

--- demo-dataset/create_bam_paths.sh
+++ demo-dataset/create_bam_paths.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for BAM_FILE in $PWD/reduced-bam/*.CEU.exome.bam; do
-    SAMPLE_NAME=$(basename "$BAM_FILE" .CEU.exome.bam)
+for BAM_FILE in $PWD/reduced-bam/*.CEU.exome.reduced.bam; do
+    SAMPLE_NAME=$(basename "$BAM_FILE" .CEU.exome.reduced.bam)
     echo "$BAM_FILE::$SAMPLE_NAME"
 done
 
