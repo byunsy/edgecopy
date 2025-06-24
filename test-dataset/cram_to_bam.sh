62b03ea8b1fda502e6ee02980727baf0fbb49094
Sang Yoon <bsyiskl@gmail.com>
2025-06-24 03:50:08 -0400

    Update new scripts

--- test-dataset/cram_to_bam.sh
+++ test-dataset/cram_to_bam.sh
@@ -3,8 +3,10 @@
 SAMPLE=$1
 REF="hg38.fa"
 
+mkdir -p bam-files
+
 CRAM_FILE="${SAMPLE}.alt_bwamem_GRCh38DH.20150826.CEU.exome.cram"
-BAM_FILE="${SAMPLE}.CEU.exome.bam"
+BAM_FILE="bam-files/${SAMPLE}.CEU.exome.bam"
 
 echo "samtools view -b -T ${REF} -o ${BAM_FILE} ${CRAM_FILE}"
 samtools view -b -T ${REF} -o ${BAM_FILE} ${CRAM_FILE}
