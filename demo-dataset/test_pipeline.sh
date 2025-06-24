0d633b7ef80eb4f7e2d10bc5224ebc7772f00111
Sang Yoon <bsyiskl@gmail.com>
2025-06-24 14:30:28 -0400

    Update pipeline

--- demo-dataset/test_pipeline.sh
+++ demo-dataset/test_pipeline.sh
@@ -4,12 +4,12 @@
 # Prepare files
 # ============================================================================
 
-# Download hg38 reference genome
+# Download hg38 reference genome (3GB)
 # ~5 minutes
 wget -c -O hg38.fa ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa
 wget -c -O hg38.fa.fai ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa.fai
 
-# Download Parascopy's hg38 homology table
+# Download Parascopy's hg38 homology table (40MB)
 wget -c https://dl.dropboxusercontent.com/s/okzeedb6gze6zzs/homology_table_hg38.tar
 tar xf homology_table_hg38.tar
 
@@ -21,17 +21,10 @@ bash create_bam_paths.sh > 1KGP.EUR.CEU.BGI.bam.list
 # ============================================================================
 
 # (1) depth module
-# ~2 minutes using 8 cores
-edgecopy depth \
- --input 1KGP.EUR.CEU.BGI.bam.list \
- --output out-depth \
- --exon-list exons.hg38.noalt.bed \
- --reference hg38.fa \
- --hom-table homology_table/hg38.bed.gz \
- -@ 8
+# - Skipped in this demo to save time; necessary files have been precomputed
 
 # (2) agCN module
-# ~2 minutes using 8 cores
+# Under 1 minute 
 edgecopy agcn \
  --input 1KGP.EUR.CEU.BGI.bam.list \
  --depth out-depth \
@@ -41,15 +34,15 @@ edgecopy agcn \
  --reference hg38.fa \
  --hom-table homology_table/hg38.bed.gz \
  --priors priors \
- -@ 8
+ -@ 2
 
 # (3) psCN module
-# ~3 minutes using 8 cores
+# Under 1 minute
 edgecopy pscn \
---input 1KGP.EUR.CEU.BGI.bam.list \
---output out-pscn \
---loci-list example.loci.hg38.bed \
---reference hg38.fa \
---hom-table homology_table/hg38.bed.gz \
---agcn-dir out-agcn \
--@ 8
+ --input 1KGP.EUR.CEU.BGI.bam.list \
+ --output out-pscn \
+ --loci-list example.loci.hg38.bed \
+ --reference hg38.fa \
+ --hom-table homology_table/hg38.bed.gz \
+ --agcn-dir out-agcn \
+ -@ 2
