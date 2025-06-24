e6aa4698dfa569bd13ddbcbb99e16585369b5fc5
Sang Yoon <bsyiskl@gmail.com>
2025-06-24 05:17:58 -0400

    Fix filepath

--- test-dataset/cram_to_bam.sh
+++ test-dataset/cram_to_bam.sh
@@ -5,7 +5,7 @@ REF="hg38.fa"
 
 mkdir -p bam-files
 
-CRAM_FILE="${SAMPLE}.alt_bwamem_GRCh38DH.20150826.CEU.exome.cram"
+CRAM_FILE="cram-files/${SAMPLE}.alt_bwamem_GRCh38DH.20150826.CEU.exome.cram"
 BAM_FILE="bam-files/${SAMPLE}.CEU.exome.bam"
 
 echo "samtools view -b -T ${REF} -o ${BAM_FILE} ${CRAM_FILE}"
