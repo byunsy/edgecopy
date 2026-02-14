792a95d295442996f8c7fcf3aa1b3aed879f728d
byunsy <bsyiskl@gmail.com>
2026-02-14 14:26:32 -0800

    Clean up stdout logs

--- src/edgecopy/get_bam_stats.py
+++ src/edgecopy/get_bam_stats.py
@@ -58,10 +58,12 @@ def run(inp):
     
     count_matrix_file = os.path.join(inp.all_cnts_dir, 'all.counts.nondup.tsv')
     interval_file = os.path.join(inp.all_cnts_dir, 'all.counts.nondup.meta.tsv')
+    
     out_fp = inp.all_stat_fp
+    out_fp_log_file = os.path.join(inp.all_cnts_dir, 'all.stats.log.txt')
     
     er.referenceset_fit(count_matrix_file, interval_file, out_fp,
-                        logfile=sys.stdout, exons_for_beta=10000, max_ref=30, threads=int(inp.threads))
+                        logfile=out_fp_log_file, exons_for_beta=10000, max_ref=30, threads=int(inp.threads))
 
     """
     # Get a list of input BAM filepaths
